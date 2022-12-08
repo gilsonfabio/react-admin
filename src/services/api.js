26e7f56b9f96717356e500d02e8e5c3ac8c3a310
Gilson Fábio <gilsonfabio@gmail.com>
2022-12-08 11:18:51 -0300

    Alteração da chamada da api https

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 //const api = axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com' });
 //const api = axios.create({baseURL: 'http://localhost:3333' });
-const api = axios.create({baseURL: 'http://82.180.162.1:3333' });
+const api = axios.create({baseURL: 'https://82.180.162.1:3333' });
 export default api;
