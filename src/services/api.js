23943fbd09aff3200c173328a98baeb1a3e233de
Gilson Fábio <gilsonfabio@gmail.com>
2023-01-09 09:28:17 -0300

    Alteração da chamada da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 //const api = axios.create({baseURL: 'https://innvento2120.c37.integrator.host' });
 //const api = axios.create({baseURL: 'http://localhost:3333' });
-const api = axios.create({baseURL: 'https://sindicaldas.com.br/api' });
+const api = axios.create({baseURL: 'http://api'});
 export default api;
