1c908b07f127d4b5b64762d961f6be8d2ec4f411
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2024-10-08 15:31:12 -0300

    Localiza Secretaria pela descrição

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//const api = axios.create({baseURL: 'https://backcaldascard.vercel.app/' });
-const api = axios.create({baseURL: 'http://localhost:3333' });
+const api = axios.create({baseURL: 'https://backcaldascard.vercel.app/' });
+//const api = axios.create({baseURL: 'http://localhost:3333' });
 //const api = axios.create({baseURL: 'http://82.180.162.1:3333' });
 export default api;
