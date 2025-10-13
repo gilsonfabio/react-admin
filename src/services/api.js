4ff62f76baf15a790d6d7c69b88f15a270f94ef5
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2025-10-13 10:32:08 -0300

    Mudança no status de Bloqueados no Extrato

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 
 const api = axios.create({baseURL: 'https://backcaldascard.vercel.app/' });
 //const api = axios.create({baseURL: 'http://localhost:3333' });
-//const api = axios.create({baseURL: 'http://82.180.162.1:3333' });
 export default api;
